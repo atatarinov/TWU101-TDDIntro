48a7d2e5747101468880ffcc76e11d6ff15e8f9e
Alexander Tatarinov <tatarinov.alexander@gmail.com>
2018-07-11 11:10:42 -0400

    passes first account test

--- src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
@@ -2,4 +2,9 @@ package com.thoughtworks.tddintro.exercises.accountbalance;
 
 public class Account {
 
+    public int balance;
+
+    public void deposit(int depositAmount) {
+        balance += depositAmount;
+    }
 }
--- src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
@@ -3,22 +3,28 @@ package com.thoughtworks.tddintro.exercises.accountbalance;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class AccountTests {
-    @Ignore  // Remove each @Ignore and implement test
+
     @Test
-    public void shouldIncreaseMyBalanceWhenIDepositMoney(){
+    public void shouldIncreaseMyBalanceWhenIDepositMoney() {
         Account account = new Account();
+        account.balance = 100;
+        account.deposit(50);
+
+        assertThat(account.balance, is(150));
     }
 
     @Test
     @Ignore  // Remove each @Ignore and implement test
-    public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
-
+    public void shouldDecreaseMyBalanceWhenIWithdrawMoney() {
     }
 
     @Test
     @Ignore  // Remove each @Ignore and implement test
-    public void shouldNotDecreaseMyBalanceWhenIWithdrawMoneyAndDoNotHaveEnoughToCoverTheWithdrawal(){
+    public void shouldNotDecreaseMyBalanceWhenIWithdrawMoneyAndDoNotHaveEnoughToCoverTheWithdrawal() {
 
     }
 }
